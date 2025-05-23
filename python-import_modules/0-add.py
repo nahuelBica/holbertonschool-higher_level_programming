cc77eb83577ae7a3d888b07dc51d0a7ac39be711
Nahuel Bica <nahuelbica5912@gmail.com>
2025-05-23 12:35:50 -0300

    task 0 - fix case with import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-
-a = 1
-b = 2
-suma = add(a, b)
-print(f"{a} + {b} = {suma}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    suma = add(a, b)
+    print(f"{a} + {b} = {suma}")
