66651303f4c3888f7b9571eb0055d2185ef68c5f
Nahuel Bica <nahuelbica5912@gmail.com>
2025-05-23 14:50:41 -0300

    task 2 args - fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,8 @@ def print_args():
     ac = len(al)
     msj = ("0 arguments." if ac == 0 else "1 argument:" if ac == 1 else f"{ac} arguments:")
     print(msj)
-    for arg in al:
-        print(f"{i}: {arg}" for i , arg in enumerate(al, start=1))
+    for i,arg in enumerate(al, start=1):
+        print(f"{i}: {arg}")
 
 if __name__ == "__main__":
     print_args()
