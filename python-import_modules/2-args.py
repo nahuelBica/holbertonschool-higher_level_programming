4d0e8fb33f3b8e00ac669e7a26b66ece5b664aaa
Nahuel Bica <nahuelbica5912@gmail.com>
2025-05-23 14:54:22 -0300

    task 2 - pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,12 @@ import sys
 def print_args():
     al = sys.argv[1:]
     ac = len(al)
-    msj = ("0 arguments." if ac == 0 else "1 argument:" if ac == 1 else f"{ac} arguments:")
+    msj = ("0 arguments." if ac == 0 else "1 argument:"
+           if ac == 1 else f"{ac} arguments:")
     print(msj)
-    for i,arg in enumerate(al, start=1):
+    for i, arg in enumerate(al, start=1):
         print(f"{i}: {arg}")
+
 
 if __name__ == "__main__":
     print_args()
