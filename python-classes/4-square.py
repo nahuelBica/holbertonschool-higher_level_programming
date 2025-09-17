63b4c43b2aa6bf3a33add83150978a7875e41895
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-17 14:47:39 -0300

    creation of validate function

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,24 +8,25 @@ class Square:
     """
     Si
     """
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise (TypeError("size must be an integer"))
-        if value < 0:
-            raise (ValueError("size must be >= 0"))
-        self.__size = value
-
-    def __init__(self, size=0):
+    def validate(self, size):
         if type(size) is not int:
             raise (TypeError("size must be an integer"))
         if size < 0:
             raise (ValueError("size must be >= 0"))
-        self.__size = size
+        return True
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if (self.validate(value)):
+            self.__size = value
+
+    def __init__(self, size=0):
+        if (self.validate(size)):
+            self.__size = size
 
     def area(self):
         return (pow(self.__size, 2))
