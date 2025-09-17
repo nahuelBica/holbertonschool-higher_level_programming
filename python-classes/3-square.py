12b0d0a3b3ed6118e0fbae4cae8f2fb72fa5c52a
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-17 14:15:04 -0300

    task 0 - pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,19 +2,20 @@
 """
 Si
 """
+
+
 class Square:
     """
     Si
     """
     __size = 0
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if type(size) is not int:
-            raise(TypeError("size must be an integer"))
+            raise (TypeError("size must be an integer"))
         if size < 0:
-            raise(ValueError("size must be >= 0"))
+            raise (ValueError("size must be >= 0"))
         self.__size = size
-    
+
     def area(self):
-        return(pow(self.__size,2))
-    
+        return (pow(self.__size , 2))
