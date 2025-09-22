770c6f2d42625f4d84ef99663e03c7bfe89eff08
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-21 22:18:07 -0300

    task 7 - variable symbol - fixed

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
     def __str__(self):
         if (self.__width == 0 or self.__height == 0):
             return ""
-        return "\n".join([f"{Rectangle.print_symbol}" * self.__width for i in range(self.__height)])
+        return "\n".join([f"{self.print_symbol}" * self.__width for i in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
