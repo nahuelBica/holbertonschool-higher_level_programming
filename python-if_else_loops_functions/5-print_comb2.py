4aacf114cc7b9f9fef7b5edcd5a1fa1a81c2252d
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-21 22:53:06 -0300

    task 5 - added format string

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i != 99:
-        print(f"0{i}" if i < 10 else f"{i}", end=", ")
+        print("{:02}".format(i) if i < 10 else f"{i}", end=", ")
     else:
-        print(f"{i}")
+        print("{}".format(i))
