8f8f07b761c0ef416cc60996ce2474e14e58c63c
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-28 20:02:51 -0300

    task 6 - base geo with area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module creates a class Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class will be a BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
