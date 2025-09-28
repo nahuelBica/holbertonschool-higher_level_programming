bffa57583b1902fca6b042c756c116c400c4fa73
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-28 20:27:46 -0300

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module creates a class Rectangle
+"""
+Bg = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(Bg):
+    """
+    Class will be a Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("heignt", height)
+        self.__width = width
+        self.__height = height
+
+
