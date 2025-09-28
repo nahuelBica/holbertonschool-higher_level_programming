cd841ed084e70c00e4952730e712996a71261ae1
Nahuel Bica <nahuelbica5912@gmail.com>
2025-09-28 19:26:50 -0300

    task 2 - class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function used to check if an object is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function used to check if an object is an instance of a class
+    """
+    return isinstance(obj, a_class)
