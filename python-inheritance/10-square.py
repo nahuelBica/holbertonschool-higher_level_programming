9055fe490e2dffeaddb0509432a1bbde61b15631
Nahuel Bica <nahuelbica5912@gmail.com>
2025-10-02 10:19:21 -0300

    task 10 - square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module creates a class Square
+"""
+Rg = __import__("9-rectangle.py").Rectangle
+
+
+class Square(Rg):
+    """
+    Class will be a Square
+    """
+    def __init__(self, size):
+        super.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return pow(self.__size, 2)
