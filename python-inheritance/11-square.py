a44488a1181200b70bbd9346250d9ce222e5556f
Nahuel Bica <nahuelbica5912@gmail.com>
2025-10-02 10:36:00 -0300

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,4 +14,4 @@ class Square(Rg):
         super().__init__(size, size)
 
     def __str__(self):
-        return  f"[Square] {self._Rg__width:d}/{self._Rg__height:d}"
+        return  f"[Square] {super().__width:d}/{super().__height:d}"
