d1b5a06d052f9ba62baa0a4efa4223cecd3c65ef
Nahuel Bica <nahuelbica5912@gmail.com>
2025-10-05 22:50:14 -0300

    task 3 and 4 - pep8

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -3,6 +3,7 @@
 """To json"""
 import json
 
+
 def to_json_string(my_obj):
     """Return json of an object"""
     return json.dumps(my_obj)
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 """From json"""
 import json
 
+
 def from_json_string(my_str):
     """Return object from json"""
     return json.loads(my_str)
