d523a703b6637463f213884f8e8114cd49a77062
Nahuel Bica <nahuelbica5912@gmail.com>
2025-10-06 13:11:13 -0300

    task 10 to 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def fizzbuzz():
+    for i in range(100):
+        to_print = i+1
+        if (i+1) % 5 == 0 and (i+1) % 3 == 0:
+            to_print = "FizzBuzz"
+        elif (i+1) % 5 == 0:
+            to_print = "Buzz"
+        elif (i+1) % 3 == 0:
+            to_print = "Fizz"
+        print("{}".format(to_print), end=" ")
