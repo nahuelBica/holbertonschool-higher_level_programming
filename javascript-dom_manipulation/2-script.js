b94ba438f99fab3014fc25ba73cf2e9251ef636d
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-10 02:09:55 -0300

    task 1 - fixed event listener task 2

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('#red_header');
 button.addEventListener('click', function () {
   const header = document.querySelector('header');
-  header.style.color = '#FF0000';
+  header.classList.add('red');
 });
