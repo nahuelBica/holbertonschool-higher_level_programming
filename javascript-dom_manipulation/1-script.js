34c0e9022d939905b029e346b75de78f2f1d1390
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-10 02:05:35 -0300

    task 0 - fixed semistandard task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('#red_header');
+button.addEventListener(function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
