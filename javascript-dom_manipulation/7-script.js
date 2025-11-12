df8ffa9af718f5cebe0d88453a1f460c0f5b69a0
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-12 12:03:24 -0300

    task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,21 @@
+fetchMovies();
+
+async function fetchMovies () {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  const listMovies = document.querySelector('ul #list_movies');
+
+  try {
+    const r = fetch(url);
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    const movieData = await r.json();
+    movieData.forEach(movie => {
+        const newMovie = document.createElement('li');
+        newMovie.textContent = movie.title;
+        listMovies.appendChild(newMovie);
+    });
+  } catch (e) {
+    console.log('Error: ', e);
+  }
+}
