ff00f0c88af47b440ad2790bf18a13c1dbcbb9a1
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-12 13:20:56 -0300

    task 7 - fixed0

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -2,15 +2,15 @@ fetchMovies();
 
 async function fetchMovies () {
   const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
-  const listMovies = document.querySelector('ul #list_movies');
+  const listMovies = document.querySelector('ul#list_movies');
 
   try {
-    const r = fetch(url);
+    const r = await fetch(url);
     if (!r.ok) {
       throw new Error(r.status);
     }
     const movieData = await r.json();
-    movieData.forEach(movie => {
+    movieData.results.forEach(movie => {
         const newMovie = document.createElement('li');
         newMovie.textContent = movie.title;
         listMovies.appendChild(newMovie);
