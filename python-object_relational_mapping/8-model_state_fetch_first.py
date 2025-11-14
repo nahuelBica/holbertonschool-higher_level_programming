e9d469b9c6af2c21a44ead92e34a775acb09a580
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-14 14:13:00 -0300

    task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script used to get all states from db
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    av = sys.argv[1:]
+    engine = create_engine(
+        f'mysql+mysqldb://{av[0]}:{av[1]}@localhost/{av[2]}',
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(State.id == 1)
+    print('{}: {}'.format(state.id, state.name))
