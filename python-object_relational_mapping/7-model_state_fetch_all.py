e25114a1bbddf1f3220c8cc131754790bd66960c
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-14 14:02:16 -0300

    task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script used to get all states from db
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    av = sys.argv[1:]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session(engine)
+
+    states = session.query(State).order_by(State.id).all()
+
+    for s in states:
+        print('{}: {}'.format(s.id, s.name))
