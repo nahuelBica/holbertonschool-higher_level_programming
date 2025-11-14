9fce5b228bc28fa349566f5b3f667d15eff77b42
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-14 10:21:06 -0300

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
                            charset="utf8")
     string = av[3]
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%s{}'"
-                "ORDER BY id ASC;".format(string))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%s'"
+                "ORDER BY id ASC;", (string))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
