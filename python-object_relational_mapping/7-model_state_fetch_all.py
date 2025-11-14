b65c70b7fe70445c474f8052d3a98c606874d9e5
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-14 14:07:33 -0300

    fixed session - task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    session = Session(engine)
+    session = Session()
 
     states = session.query(State).order_by(State.id).all()
 
