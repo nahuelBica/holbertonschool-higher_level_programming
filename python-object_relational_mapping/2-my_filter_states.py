6564f6297804815812c45b99a5cc4934b8052638
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-14 09:52:37 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script used to filter states from db
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    av = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=av[0],
+                           passwd=av[1],
+                           db=av[2],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '" + av[3] + "'"
+                "ORDER BY id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
