d64fb8e150b3fceaa3cba9a113ddbe63c6e31585
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-16 11:38:46 -0300

    task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-from extensions import read
-
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,9 +17,11 @@ def contact():
 @app.route('/items')
 def items():
       
-      file_name = "items.json"
-      items = read(file_name)
-      return render_template('items.html', items=items)
+    file_name = "items.json"
+    with open(file_name, "r") as file:
+        data = json.load(file)
+        items = data.get("items", [])
+    return render_template('items.html', items=items)
 
 if __name__ == '__main__':
        app.run(debug=True, port=5000)
