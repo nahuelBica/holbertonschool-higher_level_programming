3912a81b90540b16e61410efc85fa9293dc0136f
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-23 15:17:14 -0300

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Student with method to_json
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(
+            type(attr) is str for attr in attrs
+        ):
+            return {
+                key: getattr(self, key) for key in attrs if hasattr(self, key)
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if hasattr(self, key):
+                setattr(self, key, json[key])
