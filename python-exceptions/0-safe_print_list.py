636dc6bfde62656b14311074ea06c1b09b23eea8
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-26 00:13:34 -0300

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""function that prints x elements of a list."""
+
+
+def safe_print_list(my_list=[], x=0):
+
+    """function that prints x elements of a list."""
+
+    out = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            out += 1
+    except IndexError:
+        raise
+    print()
+    return out
